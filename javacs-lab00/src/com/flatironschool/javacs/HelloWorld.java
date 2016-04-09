aac3044422e92a8b2635a0ce209ae93037707444
Phuc Cao <hanhphuc296@gmail.com>
2016-04-08 23:34:51 -0400

    done lab0

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,14 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        Double ver;
+	String verString;
+	verString = System.getProperty("java.version");
+	ver = Double.parseDouble(verString);
+	return ver;
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	System.out.println(getVersion());
     }
 }
