2859d1d761ccdc0e8f59d2f0d818345206f4d964
Phuc Cao <hanhphuc296@gmail.com>
2016-04-08 23:58:14 -0400

    local tests not updated?

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,8 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        Double ver;
-	String verString;
-	verString = System.getProperty("java.version");
-	ver = Double.parseDouble(verString);
+	String verString = System.getProperty("java.specification.version");
+	Double ver = Double.parseDouble(verString);
 	return ver;
     }
 
